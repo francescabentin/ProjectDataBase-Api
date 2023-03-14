6f5ea56ae6ef4f79cdb4d331ff4fdb54b3a456eb
Karla <karlafernandezchirinos@gmail.com>
2023-03-14 22:56:53 +0100

    add component_form

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import user from '../images/user.jpeg';
 import logo from "../images/logo-adalab.png";
 import { useState } from 'react';
 import dataApi from '../services/Api.js';
+import Form from './form'
 
 function App() {
   const [url, setUrl] = useState('');
@@ -104,130 +105,15 @@ const handleClickCreateCard = (ev) => {
           {/*fin component card*/}
         </section>
         {/*inicio component form*/}         
-        <section className="form">
-          <h2 className="title">Información</h2>
+        <Form
+        data={data}
+        handleInput={handleInput}
+        handleClickCreateCard={handleClickCreateCard}
+        url={url}
+        />
 
-          <section className="ask-info">
-            <p className="subtitle">Cuéntanos sobre el proyecto</p>
-            <hr className="line" />
-          </section>
-
-          <fieldset className="project">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nombre del proyecto *"
-              name="name"
-              id="name"
-              value={data.name}
-              onInput={handleInput}
-              required
-            />
-            <input
-              className="input"
-              type="text"
-              name="slogan"
-              id="slogan"
-              placeholder="Slogan"
-              value={data.slogan}
-              onChange={handleInput}
-              pattern="/^[A - ZÁ - üñÑ]+$/i"
-            />
-            <div className="project-links">
-              <input
-                className="input"
-                type="text"
-                name="repo"
-                id="repo"
-                placeholder="Repo *"
-                value={data.repo}
-                onChange={handleInput}
-                required
-                pattern="/^((https?|ftp|file):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/"
-              />
-              <input
-                className="input"
-                type="text"
-                placeholder="Demo *"
-                name="demo"
-                id="demo"
-                value={data.demo}
-                onChange={handleInput}
-                required
-                pattern="/^((https?|ftp|file):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/"
-              />
-            </div>
-            <input
-              className="input"
-              type="text"
-              placeholder="Tecnologías"
-              name="technologies"
-              id="technologies"
-              value={data.technologies}
-              onChange={handleInput}
-              pattern="/^[A - ZÁ - üñÑ]+$/i"
-            />
-            <textarea
-              className="textarea"
-              type="text"
-              placeholder="Descripción *"
-              name="desc"
-              id="desc"
-              value={data.desc}
-              onChange={handleInput}
-              required
-             
-            ></textarea>
-          </fieldset>
-
-          <section className="ask-info">
-            <p className="subtitle">Cuéntanos sobre la autora</p>
-            <hr className="line" />
-          </section>
-
-          <fieldset className="autor">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nombre *"
-              name="autor"
-              id="autor"
-              value={data.autor}
-              onChange={handleInput}
-              required
-              pattern="/^[A - ZÁ - üñÑ]+$/i"
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder="Trabajo *"
-              name="job"
-              id="job"
-              value={data.job}
-              onChange={handleInput}
-              required
-              pattern="/^[A - ZÁ - üñÑ]+$/i"
-            />
-          </fieldset>
-
-          <section className="buttons-img">
-            <button className="btn">Subir foto de proyecto</button>
-            <button className="btn">Subir foto de autora</button>
-          </section>
-          <section className="buttons-img">
-           <button className="btn-large"  onClick={handleClickCreateCard}>
-              Crear Tarjeta
-            </button>
-          </section>
-
-          <section className="card">
-            <span className="linkCard"> {`La tarjeta ha sido creada: ${url}`}</span>
-            <a href="" className="" target="_blank" rel="noreferrer">
-              {url}
-            </a>
-          </section>
           {/*fin component form*/} 
-        </section>
+        {/* </section> */}
       </main>
     </div>
   );

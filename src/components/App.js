2ba892b78ea9b63ae4ad0ba4beb437475a19f7fd
Karla <karlafernandezchirinos@gmail.com>
2023-03-17 16:31:12 +0100

    add landing_createCard__LocalStorage-get

--- src/components/App.js
+++ src/components/App.js
@@ -1,108 +1,47 @@
 import '../styles/App.scss';
-import cover from '../images/cover_2.jpeg';
-import user from '../images/user.jpeg';
-import logo from "../images/logo-adalab.png";
-import { useState } from 'react';
-import dataApi from '../services/Api.js';
+import {Route,Routes} from 'react-router-dom';
+
 
 //importo lo componentes
-import ImgCard from '../components/Preview/ImgCard';
-import Card from '../components/Preview/Card';
+
 import Header from './Header';
-import Form from './Form';
+import CreateCard from './CreateCard';
+import Landing from './Landing';
 
 function App() {
-  const [url, setUrl] = useState('');
-const [data, setData] = useState ({
-  name:'',
-  slogan:'',
-  repo:'',
-  demo: '',
-  technologies:'',
-  desc:'',
-  autor:'',
-  job:'',
-  image: 'https://via.placeholder.com/140x130',
-  photo: 'https://via.placeholder.com/140x130',
-});
 
-const [isCard, setIsCard] = useState(false);
-const [isError, setIsError] = useState(false);
+// /* VARIABLES ESTADO (DATOS) */
+//   const [allMovies, setAllMovies] = useState(ls.get('cache', []));
+//   const [filterValues, setFilterValues] = useState({
+//     filterTitle: '',
+//     filterSynopsis: '',
+//   });
 
-const handleInput = (ev) => {
-  const inputValue=ev.target.value;
-  const inputName=ev.target.name;
-  const textValidation = /^[A-Za-zñÑáéíóúÁÉÍÓÚüÜïÏç.,-_\s]*$/;
-  const linkValidation = /^((https?|ftp|file):\/\/)?([\da-z.-]+).([a-z.]{2,6})([/\w .-]*)*\/?$/;
-  if (inputName === "name") {
-    setData ({...data,name:inputValue});
-  }
-  else if (inputName === "slogan") {
-    setData({...data,slogan:inputValue});
-  } else if (inputName === "repo") {
-    setData({...data,repo:inputValue});
-    if(!linkValidation.test(inputValue)){
-    }
-  } else if (inputName === "demo") {
-    setData({...data,demo:inputValue});
-    if (!linkValidation.test(inputValue)){
-      
-    }
-  } else if (inputName === "technologies") {
-    setData({...data,technologies:inputValue});
-  } else if (inputName === "desc") {
-    setData({...data,desc:inputValue});
-  } else if (inputName === "autor" && textValidation.test(inputValue)) {
-    setData({...data,autor:inputValue});
-  } else if (inputName === "job" && textValidation.test(inputValue)) {
-   setData({...data,job:inputValue});
-  }
-}
-
-const handleClickCreateCard = (ev) => {
-  ev.preventDefault();
-  console.log(data);
-  dataApi(data)
-  .then(info => {
-        console.log (info);
-    setUrl(info.cardURL);
-    setIsCard(true);
-    if (info.success) {
-      setIsError(true);
-    } else {
-      setIsError(false);
-    }
-  })
-}
+//   /* EFECTOS */
+//   useEffect(() => {
+//     // Código que solo se ejecuta una vez
+//     if (ls.notIncludes('cache')) {
+//       obj.fetchMovies().then((data) => {
+//         // Guardarlo en una variable.
+//         setAllMovies(data);
+//         ls.set('cache', data);
+//       });
+//     }
+//   }, []);  
 
   return (
     <div className="container">
       <Header/>
-      <main className="main">
-        {/* component preview */}
-        <section className="preview">
-          { /* component image*/}
-          <ImgCard
-          cover={cover}
-          />
-          <Card
-          data={data}
-          user={user}
-          />
-         </section>
-          {/*inicio component card* selecciono <section className="autor"> porque el componente me pide un padre para el chiquillo  */} 
-        {/*inicio component form*/}         
-        <Form
-        data={data}
-        handleInput={handleInput}
-        handleClickCreateCard={handleClickCreateCard}
-        url={url}
-        isCard={isCard}
-        isError={isError}
-        />
-          {/*fin component form*/} 
-        {/* </section> */}
-      </main>
+      <Routes>
+        <Route path="/"
+        element={<Landing/>}>
+        </Route>
+        <Route path='/CreateCard'
+        element={<CreateCard />}>
+        </Route>
+      </Routes>
+
+     
     </div>
   );
 }

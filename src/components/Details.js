c04021034cbd8cbc5d3b6bb9209592724e576094
Patuco <patricia.beco.dg@gmail.com>
2023-03-30 18:04:53 +0200

    Add details styles and proyectos canel logo

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,21 +1,55 @@
 import cover from "../images/cover.jpeg";
 import user from "../images/user.jpeg";
-import Card from "./Preview/Card";
-import ImgCard from "../components/Preview/ImgCard";
 
-function Details({ data}) {
+function Details() {
   return (
-    <section>
-      <ImgCard
-        className={"preview__cover-img"}
-        data={data}
-        defaultAvatar={cover}
-      />
-      <Card
-        className={"preview__card--autor--img"}
-        data={data}
-        defaultAvatar={user}
-      />
+    <section className="detailsPage">
+      <img className="detailsPage__cover" src={cover} alt="" />
+
+      <section className="detailsPage__card">
+        <section className="detailsPage__card--info">
+          <div className="detailsPage__card--info--intro">
+            <p className="detailsPage__card--info--subtitle">
+              Personal Project Card
+            </p>
+            <hr className="detailsPage__card--info--line" />
+          </div>
+          <h2 className="detailsPage__card--info--title">
+            {"Elegant Workspace"}
+          </h2>
+          <p className="detailsPage__card--info--slogan">
+            {"Diseños Exclusivos"}
+          </p>
+          <p className="detailsPage__card--info--desc">
+            {" "}
+            {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet faucibus commodo tellus lectus lobortis."}
+          </p>
+          <section className="detailsPage__card--info--technologies">
+            <p className="text">
+              {" "}
+              {"React - JS - MongoDB"}
+            </p>
+          </section>
+          <div className="detailsPage__card--info--icons">
+            <a href="#" target="_blank" rel="noreferrer">
+              <i class="detailsPage__card--info--icons--i fa-brands fa-github"></i>
+            </a>
+            <a href="#" target="_blank" rel="noreferrer">
+              <i class="detailsPage__card--info--icons--i fa-solid fa-globe"></i>
+            </a>
+          </div>
+        </section>
+
+        <section className="detailsPage__card--autor">
+          <img className="detailsPage__card--autor--img" src={user} alt="" />
+          <p className="detailsPage__card--autor--job">
+            {"Full Stack Developer"}
+          </p>
+          <p className="detailsPage__card--autor--name">
+            {"Emmelie Björklund"}
+          </p>
+        </section>
+      </section>
     </section>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,14 @@
-//import logo from "../images/logo-adalab.png";
-import boca from "../images/boca2.png"
-import nombre from "../images/nombre2.png"
+import { Link } from "react-router-dom";
+import equipo from "../images/Equipo_palique.svg"
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__project">
-        <img src={boca} alt="" className="header__boca" />
-        {/* <i className="header__project--icon fa-solid fa-laptop-code"></i> */}
-        {/* <p className="header__project--name">Proyectos Molones</p> */}
-      </div>
-      <img
-        className="header__logo"
-        src={nombre}
-        title="Adalab"
-        alt="Logo de Adalab"
-      />
+      <Link to="/" className="header__logo">
+        <img src={equipo} alt="" />
+      </Link>
+      <Link to="/" className="header__team">
+        <p>Equipo Palique</p>
+      </Link>
     </header>
   );
 };
